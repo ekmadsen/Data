74fc4d7525eefaff5962f1ba0e1fe1bdb481841d
Erik Madsen <ekmadsen@comcast.net>
2019-11-07 21:49:58 -0600

    Implemented the remaing six (of seven) IDbConnection.QueryAsync extension methods.

--- Data/ExtensionMethods.cs
+++ Data/ExtensionMethods.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Data;
 using System.Threading.Tasks;
 using Dapper;
@@ -18,20 +17,13 @@ namespace ErikTheCoder.Data
         public static async Task QueryAsync<TFirst, TSecond>(this IDbConnection Connection, string Sql, Action<TFirst, TSecond> Map, object Param = null,
             IDbTransaction Transaction = null, bool Buffered = true, string SplitOn = "Id", int? CommandTimeout = null, CommandType? CommandType = null)
         {
+            if (!Buffered) throw new Exception($"{nameof(QueryAsync)} extension method called with Map Action must be buffered.");
             Func<TFirst, TSecond, bool> map = (First, Second) =>
             {
                 Map(First, Second);
                 return true;
             };
-            IEnumerable results = await Connection.QueryAsync(Sql, map, Param, Transaction, Buffered, SplitOn, CommandTimeout, CommandType);
-            if (!Buffered)
-            {
-                // Must traverse results to call map func on each row returned by query.
-                // ReSharper disable once NotAccessedVariable
-                bool lastResult;
-                // ReSharper disable once RedundantAssignment
-                foreach (bool result in results) lastResult = result; // Prevent for loop from being removed by compiler as a no-op.
-            }
+            await Connection.QueryAsync(Sql, map, Param, Transaction, true, SplitOn, CommandTimeout, CommandType);
         }
 
 
@@ -39,8 +31,13 @@ namespace ErikTheCoder.Data
         public static async Task QueryAsync<TFirst, TSecond, TThird>(this IDbConnection Connection, string Sql, Action<TFirst, TSecond, TThird> Map, object Param = null,
             IDbTransaction Transaction = null, bool Buffered = true, string SplitOn = "Id", int? CommandTimeout = null, CommandType? CommandType = null)
         {
-            Action<TFirst, TSecond, TThird, object, object, object, object> map = (First, Second, Third, Fourth, Fifth, Sixth, Seventh) => Map(First, Second, Third);
-            await QueryAsync(Connection, Sql, map, Param, Transaction, Buffered, SplitOn, CommandTimeout, CommandType);
+            if (!Buffered) throw new Exception($"{nameof(QueryAsync)} extension method called with Map Action must be buffered.");
+            Func<TFirst, TSecond, TThird, bool> map = (First, Second, Third) =>
+            {
+                Map(First, Second, Third);
+                return true;
+            };
+            await Connection.QueryAsync(Sql, map, Param, Transaction, true, SplitOn, CommandTimeout, CommandType);
         }
 
 
@@ -48,8 +45,13 @@ namespace ErikTheCoder.Data
         public static async Task QueryAsync<TFirst, TSecond, TThird, TFourth>(this IDbConnection Connection, string Sql, Action<TFirst, TSecond, TThird, TFourth> Map, object Param = null,
             IDbTransaction Transaction = null, bool Buffered = true, string SplitOn = "Id", int? CommandTimeout = null, CommandType? CommandType = null)
         {
-            Action<TFirst, TSecond, TThird, TFourth, object, object, object> map = (First, Second, Third, Fourth, Fifth, Sixth, Seventh) => Map(First, Second, Third, Fourth);
-            await QueryAsync(Connection, Sql, map, Param, Transaction, Buffered, SplitOn, CommandTimeout, CommandType);
+            if (!Buffered) throw new Exception($"{nameof(QueryAsync)} extension method called with Map Action must be buffered.");
+            Func<TFirst, TSecond, TThird, TFourth, bool> map = (First, Second, Third, Fourth) =>
+            {
+                Map(First, Second, Third, Fourth);
+                return true;
+            };
+            await Connection.QueryAsync(Sql, map, Param, Transaction, true, SplitOn, CommandTimeout, CommandType);
         }
 
 
@@ -57,8 +59,13 @@ namespace ErikTheCoder.Data
         public static async Task QueryAsync<TFirst, TSecond, TThird, TFourth, TFifth>(this IDbConnection Connection, string Sql, Action<TFirst, TSecond, TThird, TFourth, TFifth> Map, object Param = null,
             IDbTransaction Transaction = null, bool Buffered = true, string SplitOn = "Id", int? CommandTimeout = null, CommandType? CommandType = null)
         {
-            Action<TFirst, TSecond, TThird, TFourth, TFifth, object, object> map = (First, Second, Third, Fourth, Fifth, Sixth, Seventh) => Map(First, Second, Third, Fourth, Fifth);
-            await QueryAsync(Connection, Sql, map, Param, Transaction, Buffered, SplitOn, CommandTimeout, CommandType);
+            if (!Buffered) throw new Exception($"{nameof(QueryAsync)} extension method called with Map Action must be buffered.");
+            Func<TFirst, TSecond, TThird, TFourth, TFifth, bool> map = (First, Second, Third, Fourth, Fifth) =>
+            {
+                Map(First, Second, Third, Fourth, Fifth);
+                return true;
+            };
+            await Connection.QueryAsync(Sql, map, Param, Transaction, true, SplitOn, CommandTimeout, CommandType);
         }
 
 
@@ -66,8 +73,13 @@ namespace ErikTheCoder.Data
         public static async Task QueryAsync<TFirst, TSecond, TThird, TFourth, TFifth, TSixth>(this IDbConnection Connection, string Sql, Action<TFirst, TSecond, TThird, TFourth, TFifth, TSixth> Map, object Param = null,
             IDbTransaction Transaction = null, bool Buffered = true, string SplitOn = "Id", int? CommandTimeout = null, CommandType? CommandType = null)
         {
-            Action<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, object> map = (First, Second, Third, Fourth, Fifth, Sixth, Seventh) => Map(First, Second, Third, Fourth, Fifth, Sixth);
-            await QueryAsync(Connection, Sql, map, Param, Transaction, Buffered, SplitOn, CommandTimeout, CommandType);
+            if (!Buffered) throw new Exception($"{nameof(QueryAsync)} extension method called with Map Action must be buffered.");
+            Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, bool> map = (First, Second, Third, Fourth, Fifth, Sixth) =>
+            {
+                Map(First, Second, Third, Fourth, Fifth, Sixth);
+                return true;
+            };
+            await Connection.QueryAsync(Sql, map, Param, Transaction, true, SplitOn, CommandTimeout, CommandType);
         }
 
 
@@ -75,20 +87,13 @@ namespace ErikTheCoder.Data
         public static async Task QueryAsync<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh>(this IDbConnection Connection, string Sql, Action<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh> Map, object Param = null,
             IDbTransaction Transaction = null, bool Buffered = true, string SplitOn = "Id", int? CommandTimeout = null, CommandType? CommandType = null)
         {
+            if (!Buffered) throw new Exception($"{nameof(QueryAsync)} extension method called with Map Action must be buffered.");
             Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, bool> map = (First, Second, Third, Fourth, Fifth, Sixth, Seventh) =>
             {
                 Map(First, Second, Third, Fourth, Fifth, Sixth, Seventh);
                 return true;
             };
-            IEnumerable results = await Connection.QueryAsync(Sql, map, Param, Transaction, Buffered, SplitOn, CommandTimeout, CommandType);
-            if (!Buffered)
-            {
-                // Must traverse results to call map func on each row returned by query.
-                // ReSharper disable once NotAccessedVariable
-                bool lastResult;
-                // ReSharper disable once RedundantAssignment
-                foreach (bool result in results) lastResult = result; // Prevent for loop from being removed by compiler as a no-op.
-            }
+            await Connection.QueryAsync(Sql, map, Param, Transaction, true, SplitOn, CommandTimeout, CommandType);
         }
     }
 }
